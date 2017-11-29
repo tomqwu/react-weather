017553e7f2a548510c654972e97781f17ddaf777
Tom Wu <tomwu@thoughtworks.com>
2017-11-29 11:25:43 -0500

    init

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_KEY = "ced1f3a8697e6001002f17de6b9dfb65";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${
+  API_KEY
+}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  //console.log("Request:", request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
